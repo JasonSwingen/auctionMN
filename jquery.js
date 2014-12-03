1d6caf707c0622bc087942528682f0375f6358cc
Jason Swingen <jason.swingen@gmail.com>
2014-12-03 14:56:09 -0600

    3 months worth of changes

--- jquery.js
+++ jquery.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
 	$('.currentheading').click(function() {
-		$(this).next().slideToggle(600);
+		$('.current').slideToggle(600);
 	});
 	$('.upcomingheading').click(function() {
-		$(this).next().slideToggle(600);
+		$('.upcoming').slideToggle(600);
 	});
 	$('.recentheading').click(function() {
 		$('.recent').slideToggle(600);
