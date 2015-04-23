ec8f61c0d90f19c316995f040dfadc32e96379cd
Jason Swingen <jason.swingen@gmail.com>
2015-04-23 17:12:19 -0500

    new auction and added sidebar buttons to auctions

--- jquery.js
+++ jquery.js
@@ -1,11 +1,30 @@
-$(document).ready(function() {
-	$('.currentheading').click(function() {
-		$('.current').slideToggle(600);
-	});
-	$('.upcomingheading').click(function() {
-		$('.upcoming').slideToggle(600);
-	});
-	$('.recentheading').click(function() {
-		$('.recent').slideToggle(600);
-	});
+$(document).ready(function($) { // Start of Document Ready Javascript
+ 'use strict';
+
+	// Smooth Scrolling
+	$(function() {
+    $('a[href*=#]:not([href=#])').click(function() {
+      if (location.pathname.replace(/^\//,'') === this.pathname.replace(/^\//,'') && location.hostname === this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+        if (target.length) {
+          $('html,body').animate({
+            scrollTop: target.offset().top
+          }, 600);
+          return false;
+        }
+      }
+    });
+  });
+
+	// Toggles auctions
+	// $('.currentheading').click(function() {
+	// 	$('.current').slideToggle(600);
+	// });
+	// $('.upcomingheading').click(function() {
+	// 	$('.upcoming').slideToggle(600);
+	// });
+	// $('.recentheading').click(function() {
+	// 	$('.recent').slideToggle(600);
+	// });
 })
